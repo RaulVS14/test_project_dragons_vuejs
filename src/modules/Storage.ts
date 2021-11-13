a19ca1a96205cae10d79bac1067cbf6c1447947b
Raul <raul.muser@gmail.com>
2021-11-13 17:46:08 +0200

    Add API and Storage modules

---
+++ src/modules/Storage.ts
@@ -0,0 +1,17 @@
+interface gameState {
+    gameId: string,
+    lives: number,
+    gold: number,
+    level: number,
+    score: number,
+    highScore: number,
+    turn: number
+}
+
+export function getStateFromStorage() {
+    return localStorage.getItem("state") ? localStorage.getItem("new") : null;
+}
+
+export function setStateToStorage(state: gameState) {
+    return localStorage.setItem("state", JSON.stringify(state));
+}
